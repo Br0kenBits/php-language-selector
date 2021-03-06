9b05a46b24a79cc1aaa831c5ed323da5170468aa
Br0kenBits <80176714+Br0kenBits@users.noreply.github.com>
2021-03-06 22:09:19 +0100

    Add files via upload

---
+++ lang/sv.php
@@ -0,0 +1,10 @@
+<?php
+
+// Associative array wiith id => text mapping
+$locale_text_array = [
+    // Language configuration
+    "php_locale"            => ["sv_SE.UTF-8", "sv_SE"],
+    "iso_language"          => "sv",
+    // Applications texts
+    "this_language"           => "Denna sida är på svenska!",
+];
