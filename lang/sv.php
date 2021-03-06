a4de5beceea8d1cc7e2e3cf6b194f49451bec99f
Br0kenBits <80176714+Br0kenBits@users.noreply.github.com>
2021-03-06 22:50:55 +0100

    Update sv.php

--- lang/sv.php
+++ lang/sv.php
@@ -6,5 +6,5 @@ $locale_text_array = [
     "php_locale"            => ["sv_SE.UTF-8", "sv_SE"],
     "iso_language"          => "sv",
     // Applications texts
-    "this_language"           => "Denna sida är på svenska!",
+    "this_language"           => "Denna sida är på svenska!"
 ];
