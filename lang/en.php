eb3768a84626fa2ae4bf304339d8a833515a88e6
Br0kenBits <80176714+Br0kenBits@users.noreply.github.com>
2021-03-06 22:08:58 +0100

    Create en.php

---
+++ lang/en.php
@@ -0,0 +1,10 @@
+<?php
+
+// Associative array wiith id => text mapping
+$locale_text_array = [
+    // Language configuration
+    "php_locale"            => ["en_US.UTF-8", "en_US"],
+    "iso_language"          => "en",
+    // Applications texts
+    "this_language"           => "This page is in english!",
+];
